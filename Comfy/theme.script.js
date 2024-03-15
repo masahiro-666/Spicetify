2be85c11dfb435089a3ea9ecebd3e439ced25db8
masahiro-666 <75487336+masahiro-666@users.noreply.github.com>
2024-03-16 03:43:10 +0700

    Update theme.script.js

--- Comfy/theme.script.js
+++ Comfy/theme.script.js
@@ -506,7 +506,7 @@ torefactor:
 	const Content = () => {
 		return Spicetify.React.createElement(
 			"div",
-			{ className: "comfy-settings" },
+			{ className: "Masafy-settings" },
 			Spicetify.React.createElement(Section, { name: "Colorscheme" }, [
 				{
 					type: Dropdown,
